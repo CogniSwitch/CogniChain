0f63f200c3effa17c70f62a582e6b240b76fbb4f
Saicharan <saicharans@cogniswitch.ai>
2023-11-14 14:02:32 +0530

    added the cogniswitch chains examples and source code

---
+++ cognichain/__init__.py
@@ -0,0 +1,6 @@
+"Cogniswitch chains"
+
+from cognichain.cogniswitch_answer import CogniswitchAnswerChain
+from cognichain.cogniswitch_loader import CogniswitchStoreChain
+
+__all__ = ["CogniswitchAnswerChain", "CogniswitchStoreChain"]
